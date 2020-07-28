519732bbfe87edda88ca3886e727429a3d6a8aa0
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-28 13:13:25 +0100

    Start of functional implementation. Had a bit of a struggle with establishing a module structure.

---
+++ MarkGravestock.TurtleKata/FunctionalTurtle.fs
@@ -0,0 +1,33 @@
+﻿module FunctionalTurtle
+
+open TurtleKata.Turtle
+open System
+
+let moveTurtle startPosition penState penColour angle distance =
+    let radians = convertToRadiansFromDegree angle
+    { From = startPosition; To = {x = startPosition.x + (distance * Math.Cos radians); y = startPosition.y + (distance * Math.Sin radians)} ; PenColour = penColour; PenState = penState }
+    
+let isAtPosition actual expected =
+    Math.Abs(actual.x - expected.x) < 0.00001 && Math.Abs(actual.y - expected.y) < 0.00001
+    
+module TurtleTests =
+
+    open Xunit
+    
+    [<Fact>]
+    let ``It can move some distance in a given direction`` () =
+        
+        let startPosition = { x = 0.0; y = 0.0 }
+        let angle = 0.0<Degrees>
+        let penState = PenState.Down
+        let penColour = PenColour.Black
+        let distance = 100.0
+        
+        let line = moveTurtle startPosition penState penColour angle distance 
+        
+        Assert.True(isAtPosition line.To {x = 100.0; y = 0.0})
+
+
+
+
+
--- MarkGravestock.TurtleKata/ObjectOrientedTurtleTests.fs
+++ MarkGravestock.TurtleKata/ObjectOrientedTurtleTests.fs
@@ -1,207 +1,215 @@
-module ObjectOrientedTurtle
+module TurtleKata
+module Turtle =
 
-open Xunit
-open System
-open System.IO
+    open System
 
-type PenColour = Red | Black| Blue
+    type PenColour = Red | Black| Blue
 
-type PenState = Up | Down
+    type PenState = Up | Down
 
-[<Measure>]
-type Degrees
+    [<Measure>]
+    type Degrees
 
-[<Measure>]
-type Radians
+    [<Measure>]
+    type Radians
 
-type Angle = double
+    type Angle = double
 
-let convertToRadiansFromDegree angle = (Math.PI / 180.0<Degrees>) * angle
+    let convertToRadiansFromDegree angle = (Math.PI / 180.0<Degrees>) * angle
 
-type Distance = double
+    type Distance = double
 
-type Position = {
-    x: Distance
-    y: Distance
-}
+    type Position = {
+        x: Distance
+        y: Distance
+    }
 
-type Line = {
-    From: Position
-    To: Position
-    PenColour: PenColour
-    PenState: PenState
-    }    
+    type Line = {
+        From: Position
+        To: Position
+        PenColour: PenColour
+        PenState: PenState
+        }    
 
-type LineWriter =
-    abstract member WriteTo: List<Line> -> Unit
-
-type HtmlSvgWriter(fileName) =
+module ObjectOrientedTurtle =
+    open Turtle
+    open System
+    open System.IO
     
-    interface LineWriter with
-        member this.WriteTo list =
-            
-            let lineToSvg line = 
-                let rgb =
-                    match line.PenColour with
-                        | PenColour.Black -> "(0,0,0)"
-                        | PenColour.Red -> "(255,0,0)"
-                        | PenColour.Blue -> "(0,0,255)"
-                        | _ -> failwith "Invalid Colour"
-                        
-                let svg = sprintf "<line x1='%f' y1='%f' x2='%f' y2='%f' style='stroke:rgb%s;stroke-width:2' />" line.From.x line.From.y line.To.x line.To.y rgb
-                svg
+    type LineWriter =
+        abstract member WriteTo: List<Line> -> Unit
+
+    type HtmlSvgWriter(fileName) =
+        
+        interface LineWriter with
+            member this.WriteTo list =
                 
-            let drawLine line = line.PenState = PenState.Down    
+                let lineToSvg line = 
+                    let rgb =
+                        match line.PenColour with
+                            | PenColour.Black -> "(0,0,0)"
+                            | PenColour.Red -> "(255,0,0)"
+                            | PenColour.Blue -> "(0,0,255)"
+                            
+                    let svg = sprintf "<line x1='%f' y1='%f' x2='%f' y2='%f' style='stroke:rgb%s;stroke-width:2' />" line.From.x line.From.y line.To.x line.To.y rgb
+                    svg
+                    
+                let drawLine line = line.PenState = PenState.Down    
 
-            let lineText = list |> List.filter drawLine |> List.map lineToSvg |> List.fold (+) ""
-            let fileContents = "<html><body><svg height='210' width='500'>" + lineText + "</svg></body></html>"
-            File.WriteAllText (fileName, fileContents) 
+                let lineText = list |> List.filter drawLine |> List.map lineToSvg |> List.fold (+) ""
+                let fileContents = "<html><body><svg height='210' width='500'>" + lineText + "</svg></body></html>"
+                File.WriteAllText (fileName, fileContents) 
 
-
-type Turtle() =
-    
-    let mutable lines = List.empty
-    
-    let mutable x = 0.0
-    let mutable y = 0.0
-    
-    let mutable penColour = PenColour.Black
-    
-    let mutable angle = 0.0<Degrees>
-    
-    let mutable penState = PenState.Down
-    
-    member this.Move (distance:Distance) =
-        let startX = x
-        let startY = y
-        let radians = convertToRadiansFromDegree angle
-        x <- x + (distance * Math.Cos radians)
-        y <- y + (distance * Math.Sin radians)
+    type Turtle() =
         
-        let line = { From = {x = startX; y = startY}; To = {x = x; y = y} ; PenColour = penColour; PenState = penState }
-        lines <- List.append lines [line]
-
-    member this.TurnClockwise degreesToTurn =
-        angle <- (angle + degreesToTurn) % 360.0<Degrees>
-
-    member this.TurnAntiClockwise degreesToTurn =
-        this.TurnClockwise -degreesToTurn
+        let mutable lines = List.empty
+        
+        let mutable x = 0.0
+        let mutable y = 0.0
+        
+        let mutable penColour = PenColour.Black
+        
+        let mutable angle = 0.0<Degrees>
+        
+        let mutable penState = PenState.Down
+        
+        member this.Move (distance:Distance) =
+            let startX = x
+            let startY = y
+            let radians = convertToRadiansFromDegree angle
+            x <- x + (distance * Math.Cos radians)
+            y <- y + (distance * Math.Sin radians)
             
-    member this.IsAtLocation(expectedX, expectedY) =
-        Math.Abs(x - expectedX) < 0.00001 && Math.Abs(y - expectedY) < 0.00001
-    
-    member this.PenUp =
-        penState <- PenState.Up
+            let line = { From = {x = startX; y = startY}; To = {x = x; y = y} ; PenColour = penColour; PenState = penState }
+            lines <- List.append lines [line]
+
+        member this.TurnClockwise degreesToTurn =
+            angle <- (angle + degreesToTurn) % 360.0<Degrees>
+
+        member this.TurnAntiClockwise degreesToTurn =
+            this.TurnClockwise -degreesToTurn
+                
+        member this.IsAtLocation(expectedX, expectedY) =
+            Math.Abs(x - expectedX) < 0.00001 && Math.Abs(y - expectedY) < 0.00001
         
-    member this.PenDown =
-        penState <- PenState.Down
+        member this.PenUp =
+            penState <- PenState.Up
+            
+        member this.PenDown =
+            penState <- PenState.Down
+            
+        member this.DrewLine =
+            penState = PenState.Down
         
-    member this.DrewLine =
-        penState = PenState.Down
-    
-    member this.SetPenColour colour =
-        penColour <- colour
+        member this.SetPenColour colour =
+            penColour <- colour
+            
+        member this.HasPenColour colour =
+            penColour = colour
         
-    member this.HasPenColour colour =
-        penColour = colour
+        member this.Write (writer:LineWriter) =
+            writer.WriteTo lines
+
+module ObjectOrientedTurtleTests =
+
+    open Xunit
+    open Turtle
+    open ObjectOrientedTurtle
     
-    member this.Write (writer:LineWriter) =
-        writer.WriteTo lines
+    [<Fact>]
+    let ``It can move some distance in the current direction`` () =
         
-[<Fact>]
-let ``It can move some distance in the current direction`` () =
-    
-    let turtle = Turtle()
-    
-    turtle.Move 100.0
-    
-    Assert.True(turtle.IsAtLocation(100.0, 0.0))
-
-[<Fact>]
-let ``It can Turn a certain number of degrees clockwise`` () =
-    
-    let turtle = Turtle()
-    
-    turtle.TurnClockwise 90.0<Degrees>
-
-    turtle.Move 100.0
-    
-    Assert.True(turtle.IsAtLocation(0.0, 100.0))
-
-[<Fact>]
-let ``It can Turn a certain number of degrees anticlockwise`` () =
-    
-    let turtle = Turtle()
-    
-    turtle.TurnAntiClockwise 90.0<Degrees>
-
-    turtle.Move 100.0
-    
-    Assert.True(turtle.IsAtLocation(0.0, -100.0))
-
-[<Fact>]
-let ``It can Put the pen up and draws no line`` () =
-    
-    let turtle = Turtle()
-    
-    turtle.PenUp
-    turtle.Move 100.0
-    
-    Assert.False(turtle.DrewLine)
-
-[<Fact>]
-let ``It can Put the pen down and draws a line`` () =
-    
-    let turtle = Turtle()
-    
-    turtle.PenDown
-    turtle.Move 100.0
-    
-    Assert.True(turtle.DrewLine)
-
-[<Fact>]
-let ``It can draw line in Red`` () =
-    
-    let turtle = Turtle()
-    
-    turtle.SetPenColour PenColour.Red
-    
-    Assert.True(turtle.HasPenColour PenColour.Red)
-
-[<Fact>]
-let ``It can draw lines in SVG`` () =
-    
-    let turtle = Turtle()
-    
-    turtle.Move 100.0
-    turtle.SetPenColour PenColour.Red
-    turtle.TurnClockwise 90.0<Degrees>
-    turtle.Move 100.0
-    turtle.PenUp
-    turtle.TurnClockwise 90.0<Degrees>
-    turtle.Move 100.0
-    turtle.PenDown
-    turtle.SetPenColour PenColour.Blue
-    turtle.TurnClockwise 90.0<Degrees>
-    turtle.Move 100.0    
-    
-    turtle.Write (HtmlSvgWriter(".\Test.html"))
-
-let drawPolygon (turtle: Turtle) sides =
-
-    let turnAngle = 360.0<Degrees> / float sides
-    
-    for i in [1..sides] do
+        let turtle = Turtle()
+        
         turtle.Move 100.0
-        turtle.TurnClockwise turnAngle
-    
+        
+        Assert.True(turtle.IsAtLocation(100.0, 0.0))
 
-[<Fact>]
-let ``It can draw a heptagon in SVG`` () =
-    
-    let turtle = Turtle()
-    
-    drawPolygon turtle 7
-    
-    turtle.Write (HtmlSvgWriter(".\Heptagon.html"))
+    [<Fact>]
+    let ``It can Turn a certain number of degrees clockwise`` () =
+        
+        let turtle = Turtle()
+        
+        turtle.TurnClockwise 90.0<Degrees>
+
+        turtle.Move 100.0
+        
+        Assert.True(turtle.IsAtLocation(0.0, 100.0))
+
+    [<Fact>]
+    let ``It can Turn a certain number of degrees anticlockwise`` () =
+        
+        let turtle = Turtle()
+        
+        turtle.TurnAntiClockwise 90.0<Degrees>
+
+        turtle.Move 100.0
+        
+        Assert.True(turtle.IsAtLocation(0.0, -100.0))
+
+    [<Fact>]
+    let ``It can Put the pen up and draws no line`` () =
+        
+        let turtle = Turtle()
+        
+        turtle.PenUp
+        turtle.Move 100.0
+        
+        Assert.False(turtle.DrewLine)
+
+    [<Fact>]
+    let ``It can Put the pen down and draws a line`` () =
+        
+        let turtle = Turtle()
+        
+        turtle.PenDown
+        turtle.Move 100.0
+        
+        Assert.True(turtle.DrewLine)
+
+    [<Fact>]
+    let ``It can draw line in Red`` () =
+        
+        let turtle = Turtle()
+        
+        turtle.SetPenColour PenColour.Red
+        
+        Assert.True(turtle.HasPenColour PenColour.Red)
+
+    [<Fact>]
+    let ``It can draw lines in SVG`` () =
+        
+        let turtle = Turtle()
+        
+        turtle.Move 100.0
+        turtle.SetPenColour PenColour.Red
+        turtle.TurnClockwise 90.0<Degrees>
+        turtle.Move 100.0
+        turtle.PenUp
+        turtle.TurnClockwise 90.0<Degrees>
+        turtle.Move 100.0
+        turtle.PenDown
+        turtle.SetPenColour PenColour.Blue
+        turtle.TurnClockwise 90.0<Degrees>
+        turtle.Move 100.0    
+        
+        turtle.Write (HtmlSvgWriter(".\Test.html"))
+
+    let drawPolygon (turtle: Turtle) sides =
+
+        let turnAngle = 360.0<Degrees> / float sides
+        
+        for i in [1..sides] do
+            turtle.Move 100.0
+            turtle.TurnClockwise turnAngle
+        
+
+    [<Fact>]
+    let ``It can draw a heptagon in SVG`` () =
+        
+        let turtle = Turtle()
+        
+        drawPolygon turtle 7
+        
+        turtle.Write (HtmlSvgWriter(".\Heptagon.html"))
 
