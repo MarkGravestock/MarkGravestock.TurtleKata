ec6ffccf8d9db7a9f93d081b480474f9568d0de0
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-08-03 09:27:19 +0100

    Change to create add turtle move function to build list of state.

--- MarkGravestock.TurtleKata/FunctionalTurtle.fs
+++ MarkGravestock.TurtleKata/FunctionalTurtle.fs
@@ -3,12 +3,22 @@
 open TurtleKata.Turtle
 open System
 
+type Move = {
+    angle: float<Degrees>
+    distance: Distance
+}
+
 let moveTurtle startPosition penState penColour angle distance =
     let radians = convertToRadiansFromDegree angle
     { From = startPosition; To = {x = startPosition.x + (distance * Math.Cos radians); y = startPosition.y + (distance * Math.Sin radians)} ; PenColour = penColour; PenState = penState }
 
-let moveTurtleFromLast (line: Line) penState penColour angle distance =
+let moveTurtleFromEndOfLine (line: Line) penState penColour angle distance =
     moveTurtle line.To penState penColour angle distance
+
+let addTurtleMove (lines: Line list) penState penColour angle distance =    
+    let lastLine = List.last lines
+    let nextLine = moveTurtleFromEndOfLine lastLine penState penColour angle distance
+    List.append lines [nextLine]
     
 let isAtPosition actual expected =
     Math.Abs(actual.x - expected.x) < 0.00001 && Math.Abs(actual.y - expected.y) < 0.00001
@@ -21,10 +31,10 @@ module TurtleTests =
     let ``It can create a line which represents a move of some distance in a given direction`` () =
         
         let startPosition = { x = 0.0; y = 0.0 }
+        let distance = 100.0
         let angle = 0.0<Degrees>
         let penState = PenState.Down
         let penColour = PenColour.Black
-        let distance = 100.0
         
         let line = moveTurtle startPosition penState penColour angle distance 
         
@@ -38,23 +48,21 @@ module TurtleTests =
         
         let startPosition = { x = 0.0; y = 0.0 }
         let angle = 0.0<Degrees>
+        let distance = 100.0
         let penState = PenState.Down
         let penColour = PenColour.Black
-        let distance = 100.0
         
-        let line = moveTurtle startPosition penState penColour angle distance
-        
+        let line = moveTurtle startPosition penState penColour angle distance    
         let lines = [line]
         
         let angle = 90.0<Degrees>
+        let distance = 100.0
         let penState = PenState.Down
         let penColour = PenColour.Blue
-        let distance = 100.0
-        
-        let lastLine = List.last lines
-        let nextLine = moveTurtleFromLast lastLine penState penColour angle distance
-        let newLines = List.append lines [nextLine]
-        let finalLine = List.last newLines
+                
+        let lines = addTurtleMove lines penState penColour angle distance
+
+        let finalLine = List.last lines
         
         Assert.True(isAtPosition finalLine.To {x = 100.0; y = 100.0})
         
